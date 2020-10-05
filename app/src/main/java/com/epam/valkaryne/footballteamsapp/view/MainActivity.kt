007d1186b1706d84e0dd01b75c232df74f5406c9
Valentine L <anix.mig29@gmail.com>
2020-10-05 10:41:01 +0300

    Rebased MainActivity file

--- app/src/main/java/com/epam/valkaryne/footballteamsapp/view/MainActivity.kt
+++ app/src/main/java/com/epam/valkaryne/footballteamsapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.epam.valkaryne.footballteamsapp
+package com.epam.valkaryne.footballteamsapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.epam.valkaryne.footballteamsapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
